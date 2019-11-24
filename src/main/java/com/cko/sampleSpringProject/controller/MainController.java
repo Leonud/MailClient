12ef69220028fcd9493c9b97ace8bf427fe7e7ce
Leonid <kosmynin.lenya@yandex.ru>
2019-11-24 11:15:50 +0300

    Film

--- src/main/java/com/cko/sampleSpringProject/controller/MainController.java
+++ src/main/java/com/cko/sampleSpringProject/controller/MainController.java
@@ -2,6 +2,7 @@ package com.cko.sampleSpringProject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -17,7 +18,34 @@ public class MainController {
     }
 
     @GetMapping("/game")
-    public String game(){
+    public String game()
+    {
         return "MyProject";
     }
+
+    @GetMapping("/test")
+    public String testing(@RequestParam String name)
+    {
+        System.out.println(name);
+        return "test";
+    }
+
+    @GetMapping("/allFilms")
+    public String all()
+    {
+        return "FilmsAll/allFilms";
+    }
+
+    @GetMapping("/createFilms")
+    public String create()
+    {
+        return "FilmsAll/createFilm";
+    }
+
+    @GetMapping("/editFilm")
+    public String edit()
+    {
+        return "FilmsAll/editFilm";
+    }
 }
+
