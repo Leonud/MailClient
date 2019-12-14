76fa0a2fc2fa5103856eec24acbc7c40cfda1341
Leonid <kosmynin.lenya@yandex.ru>
2019-12-14 14:16:57 +0300

    JSON

---
+++ src/main/resources/static/js/AllFilmsJS.js
@@ -0,0 +1,51 @@
+function test () {
+
+    $.ajax({
+        url: '/api/film/all',
+        type: 'get',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (film) {
+            console.log(film);
+            console.log(film.title);
+            console.log(film.rating);
+            alert('success');
+        },
+        error: function (response) {
+            alert('error');
+        }
+    });
+
+
+}
+
+$(document).ready(function (){
+    $.ajax({
+        url: '/api/film/all',
+        type: 'get',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (films) {
+            for (var i = 0; i < films.length; i++) {
+                $("#forAdd").before("<tr>" +
+                    "<td>" + films[i].id + "</td>" +
+                    "<td>" + films[i].title + "</td>" +
+                    "<td>" + films[i].rating + "</td>" +
+                    "<td>" + films[i].age + "</td>" +
+
+                    "<td>" +
+
+
+
+                    +"</td>" +
+
+                    "</td>"
+
+                )
+            }
+        },
+        error: function (response) {
+            alert('error');
+        }
+    });
+});
