b40a97a82c7de99feb4dbb7ab43a1942003f7e37
Leonid <kosmynin.lenya@yandex.ru>
2020-01-12 12:01:07 +0300

    post

--- src/main/java/com/cko/sampleSpringProject/controller/popmail.java
+++ src/main/java/com/cko/sampleSpringProject/controller/popmail.java
@@ -1,13 +1,8 @@
 package com.cko.sampleSpringProject.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +12,27 @@ import java.util.Optional;
 public class popmail {
 
     @GetMapping("/post")
-    public String create() {
+    public String post() {
         return "index";
+    }
+
+    @GetMapping("/sent")
+    public String sent() {
+        return "sent";
+    }
+
+    @GetMapping("/basket")
+    public String basket() {
+        return "basket";
+    }
+
+    @GetMapping("/favorites")
+    public String favorites() {
+        return "favorites";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
